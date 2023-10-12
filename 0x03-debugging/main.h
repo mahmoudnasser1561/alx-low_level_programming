6474bd6c0bc9150e52c26b39434a0ef17c86a871
mahmoud <mahmoudnasser1561@gmail.com>
2023-10-12 05:08:13 -0700

    -

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef MATH_H
-#define MATH_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MATH_H */
+#endif /* MAIN_H */
